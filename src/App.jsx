4259beee5f07d2155dcf6c7e357b06494091f663
Punidhasree <punidhasreesekar@gamil.com>
2025-10-25 22:31:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MyButton from './components/Mybutton'
+
+function App() {
+  return (
+    <div>
+      <MyButton />
+    </div>
+  )
+}
+
+export default App
